4f99182567a64340f6913fbb09f60a58ed6399bc
hanu_gupta <hanu.gupta@ffreedom.com>
2025-07-02 19:44:53 +0530

    preloading done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import HeroParallaxDemo from "@/components/hero-parallax-demo";
+import Head from "next/head";
 
 export default function Component() {
   // State for staggered animations and mobile menu
@@ -12,5 +13,17 @@ export default function Component() {
     setIsLoaded(true);
   }, []);
 
-  return <HeroParallaxDemo />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href="/homepageImages/img8.webp"
+          type="image/webp"
+        />
+      </Head>
+      <HeroParallaxDemo />
+    </>
+  );
 }
